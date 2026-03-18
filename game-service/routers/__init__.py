cc6b5682e4ddcb87d0e0e17b963d13f2ed00a6fc
S1lvestr <noban8520@gmail.com>
2026-03-18 16:16:52 +0600

    refactor: Remove /api/v1/game prefix from routers (T_T)

--- game-service/routers/__init__.py
+++ game-service/routers/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from routers.v1 import v1_router as v1_router
 
-api_router = APIRouter(prefix="/api")
+api_router = APIRouter()
 
 api_router.include_router(v1_router)
 
